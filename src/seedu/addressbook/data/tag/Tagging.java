dc1924ce48176162a9bb63a80e421bc9129315fa
Jun Wen <JunWen.91@gmail.com>
2016-09-08 19:12:12 +0800

    Added an Association Class Tagging.

---
+++ src/seedu/addressbook/data/tag/Tagging.java
@@ -0,0 +1,25 @@
+package seedu.addressbook.data.tag;
+
+import seedu.addressbook.data.person.Person;
+
+/**
+ * Adding or Removing tags to a person in the address book.
+ */
+
+public class Tagging {
+
+    private Person person;
+    private Tag tag;
+    private char tagSymbol;
+    
+    public Tagging(Person person, Tag tag, char tagSymbol) {
+        this.person = person;
+        this.tag = tag;
+        this.tagSymbol = tagSymbol;
+    }
+    
+    @Override
+    public String toString() {
+        return (tagSymbol + " " + person.getName() + " " + tag.tagName);
+    }
+}
