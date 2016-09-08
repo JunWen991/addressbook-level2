a9099f8d8ed51876eb32a5e0a69ae1b1f5d03a87
Jun Wen <JunWen.91@gmail.com>
2016-09-08 21:24:50 +0800

    Modified Name and Added NameTest

---
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -0,0 +1,18 @@
+package seedu.addressbook.data.person;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+public class NameTest {
+    
+    private Name name;
+
+    @Before
+    public void setup() throws IllegalValueException{
+        name = new Name("John K Smith");
+    }
+
+}
