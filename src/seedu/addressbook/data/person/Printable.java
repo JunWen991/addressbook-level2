82a1f7f15ec364b5ec3aa79a8e9f467d160353d3
Jun Wen <JunWen.91@gmail.com>
2016-09-08 18:20:03 +0800

    Added a Printable interface, Override getPrintableString in classes Name, Phone, Email, and Address.

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,12 @@
+package seedu.addressbook.data.person;
+
+/**
+ * A printable interface to print informations of Name, 
+ * Phone, Email, Address.
+ */
+
+public interface Printable {
+    
+    String getPrintableString();
+    
+}
--- src/seedu/addressbook/ui/TextUi.java
+++ src/seedu/addressbook/ui/TextUi.java
@@ -4,6 +4,7 @@ import static seedu.addressbook.common.Messages.*;
 
 import seedu.addressbook.commands.CommandResult;
 import seedu.addressbook.common.Utils;
+import seedu.addressbook.data.person.Printable;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
 import java.io.InputStream;
@@ -164,6 +165,21 @@ public class TextUi {
      */
     private static String getIndexedListItem(int visibleIndex, String listItem) {
         return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+    }
+    
+    /**
+     * Print a string of informations of Name,
+     * Phone, Email, Address.
+     */
+    public String getPrintableString(Printable... printables) {
+        
+        String printableString = "";
+        
+        for (Printable printable : printables) {
+            printableString += printable.getPrintableString() + " ";
+        }
+        
+        return printableString.trim();
     }
 
 }
